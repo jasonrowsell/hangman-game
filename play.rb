8a9b82989c844e81a0e5722f3963d50a4c6f1e8d
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-03-19 17:01:04 +0000

    changed array to hash

--- play.rb
+++ play.rb
@@ -8,34 +8,38 @@ class Hangman
     
   def words
     [
-      ["thursay", "A day of the week"],
-      ["arena", "A venue for sports"],
-      ["jazz", "A genre of music, with its roots in blues"],
-      ["brake", "Verb to slow down"],
-      ["smell", "Two-thirds of the sharks brain is dedicated to this"],
-      ["squash", "A sport involving a ball and rackets"]
+      {word: "thursay", clue: "A day of the week"},
+      {word: "arena", clue: "A venue for sports"},
+      {word: "jazz", clue: "A genre of music, with its roots in blues"},
+      {word: "brake", clue: "Verb to slow down"},
+      {word: "smell", clue: "Two-thirds of the sharks brain is dedicated to this"},
+      {word: "squash", clue: "A sport involving a ball and rackets"}
     ]
   end
   
   def make_guess
     puts "Guess a letter"
     guess = gets.chomp
+    
+    if @word[:word].include?(guess)
+      puts "Nice guess!"
+    else
+      puts "Incorrect guess!"
+    end
   end
-  
+
   def start
-    puts "Welcome to Hangman! Your word is #{@word[0].length} characters long"
-    puts "Your clue is: #{ @word[1] } "
+    puts "Welcome to Hangman! Your word is #{@word[:word].length} characters long"
+    puts "Your clue is: #{ @word[:clue] } "
     
     word_teaser = ""
-    @word[0].size.times do
+    @word[:word].size.times do
       word_teaser += "_ "
     end
     puts word_teaser
     
     make_guess
-    
-    
-    
+  
   end
   
 end
