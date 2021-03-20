e5bf7a9a7c2ea97606435da2a7c41c0eae2a70af
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-03-20 09:25:06 +0000

    Added lives to game

--- play.rb
+++ play.rb
@@ -3,6 +3,7 @@ class Hangman
   def initialize
     @letters = ('a'..'z').to_a
     @word = words.sample
+    @lives = 7
         
   end
     
@@ -13,24 +14,30 @@ class Hangman
       {word: "jazz", clue: "A genre of music, with its roots in blues"},
       {word: "brake", clue: "Verb to slow down"},
       {word: "smell", clue: "Two-thirds of the sharks brain is dedicated to this"},
-      {word: "squash", clue: "A sport involving a ball and rackets"}
+      {word: "squash", clue: "An indoor sport involving a ball and rackets"},
+      {word: "revenge", clue: "Those who want to get even"}
     ]
   end
   
   def make_guess
-    puts "Guess a letter"
-    guess = gets.chomp
-    
-    if @word[:word].include?(guess)
-      puts "Nice guess!"
-    else
-      puts "Incorrect guess!"
+    while @lives > 0 
+      puts "Guess a letter"
+      guess = gets.chomp
+      
+      if @word[:word].include?(guess)
+        puts "Nice guess!"
+      else
+        puts "Incorrect guess!"
+        @lives -= 1
+        puts "You have #{@lives} lives left"
+      end
     end
   end
 
   def start
     puts "Welcome to Hangman! Your word is #{@word[:word].length} characters long"
     puts "Your clue is: #{ @word[:clue] } "
+    
     
     word_teaser = ""
     @word[:word].size.times do
